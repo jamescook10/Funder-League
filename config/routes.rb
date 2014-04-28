09758ce367633d2f80d2909e58ff0aca868d5e99
jamesmarkcook <jamesmarkcook@gmail.com>
2014-04-28 14:58:16 +0100

    Changed dashboard route so that the dashboard uses Devise's 'current_user' method to determine the logged in user instead of the params in the route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 FunderLeague::Application.routes.draw do
 
+  get "/dashboard" => "dashboard#show"
   devise_for :players
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
--- spec/features/add_a_new_game_result_spec.rb
+++ spec/features/add_a_new_game_result_spec.rb
@@ -1,7 +1,17 @@
+require 'spec_helper'
+
 feature 'Games' do
+
+  let(:player) { create(:player) }
+
   scenario 'add a new result from the user dashboard' do
-    visit '/players/1/dashboard'
-    fill_in :game_opponent, with: "Sam Johnson"
+
+    visit '/dashboard'
+
+    fill_in :player_email, with: "sam.johnson@fundingcircle.com"
+    fill_in :player_password, with: "password"
+
+    fill_in :game_opponent, with: "James Cook"
     fill_in :game_player_score, with: 2
     fill_in :game_opponent_score, with: 3
     select "FIFA 14", from: :game_type
