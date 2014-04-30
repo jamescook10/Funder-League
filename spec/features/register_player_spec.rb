489efce43afcd4138f3bfcb54da11226f9ceb985
jamesmarkcook <jamesmarkcook@gmail.com>
2014-04-30 19:03:54 +0100

    Add conditional Register, Log In and Log out buttons to header. - Modify Register Player feature test to select the correct register button.

--- spec/features/register_player_spec.rb
+++ spec/features/register_player_spec.rb
@@ -10,7 +10,9 @@ feature 'Players' do
     fill_in :player_first_name, with: "James"
     fill_in :player_last_name, with: "Cook"
     fill_in :player_twitter, with: "imjamescook"
-    click_button "Register"
+    within('#new_player') do
+      click_button "Register"
+    end
 
     expect(page).to have_content "A message with a confirmation link has been sent to your Funding Circle email address. Please open the link to activate your account."
   end
