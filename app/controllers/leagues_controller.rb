4390c8b10e1ccd3049f944f4c909644bb9d2b870
jamesmarkcook <jamesmarkcook@gmail.com>
2014-05-12 18:04:33 +0100

    Create working show page for leagues - Each league is sorted by win % - Refactor sidebar links - Modify seeds to create 10 players

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,9 +1,17 @@
 class LeaguesController < ApplicationController
 
 	def index
+    @game_types = GameType.all
 	end
 
   def show
+    @game_types = GameType.all
     @game_type = GameType.find_by_name(params[:name])
+    @players = @game_type.players.includes(:games)
+    @players.sort! do |a,b|
+      b.win_percentage_for(@game_type) <=> a.win_percentage_for(@game_type) 
+    end
+
+
   end
 end
--- app/models/game_type.rb
+++ app/models/game_type.rb
@@ -2,4 +2,8 @@ class GameType < ActiveRecord::Base
   has_many :games
   has_many :players, -> { uniq }, through: :games
 
+  def to_param
+    name
+  end
+
 end
