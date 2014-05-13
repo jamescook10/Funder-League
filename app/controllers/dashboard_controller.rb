f635a4a694a24e68c3035e4423f84501111d4e11
jamesmarkcook <jamesmarkcook@gmail.com>
2014-05-13 18:02:01 +0100

    Create DashboardPresenter class

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,13 @@
+require "./app/presenters/dashboard_presenter"
+
 class DashboardController < ApplicationController
 
   before_action :authenticate_player!
 
   def index
     @game = Game.new
-  end
+    @game_types = GameType.all
+    @player_data = DashboardPresenter.new(current_player)
+  end 
 
 end
---
+++ app/presenters/dashboard_presenter.rb
@@ -0,0 +1,44 @@
+class DashboardPresenter
+
+  attr_reader :current_player, :game_types, :games
+
+  def initialize(current_player)
+    @game_types = GameType.all
+    @games = {}
+    @current_player = current_player
+    @win_percentages = self.win_percentages
+    set_instance_variables
+  end
+
+  def set_instance_variables
+    @game_types.each do |game_type|
+      self.class.send(:define_method, game_type.name.parameterize.underscore) { instance_variable_set("@#{game_type.name.parameterize.underscore}", {}) }
+      require 'pry'; binding.pry
+      @games[game_type.name.parameterize.underscore.to_sym] = "@#{game_type.name.parameterize.underscore}"
+    end
+  end
+
+  def position_in_league(game_type)
+    league_table = sort_by_win_percentage(game_type.players, game_type)
+    league_table.index(@current_player)
+  end
+
+  def win_percentages
+    @win_percentages ||= self.calculate_win_percentages
+  end
+
+  def calculate_win_percentages
+    win_percentages = {}
+    @game_types.each do |game_type|
+      win_percentages[game_type.name.parameterize] = @current_player.win_percentage_for(game_type)
+    end
+    win_percentages
+  end
+
+  def sort_by_win_percentage(players, game_type)
+    players.sort do |a,b|
+      b.win_percentage_for(game_type) <=> a.win_percentage_for(game_type) 
+    end
+  end
+
+end
