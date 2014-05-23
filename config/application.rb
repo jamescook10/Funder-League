0adf12817e72aef1d9512be1bbd4b2b8923f8eb2
jamesmarkcook <jamesmarkcook@gmail.com>
2014-05-23 12:17:51 +0100

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module FunderLeague
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     I18n.enforce_available_locales = false
+    config.assets.enabled = true
   end
 end
